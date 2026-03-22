398a325ec95ebb5456605af9701ad98818a0c86b
rodrigoniak <rodrigoniak@gmail.com>
2026-03-22 03:43:10 -0300

    feat: add load ports COM

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask, render_template, request
+from flask import Flask, jsonify, render_template, request
+import serial.tools.list_ports
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+class Source:
+    def status(self):
+        return "DISCONNECTED"
+    def list_ports(self):
+        ports = serial.tools.list_ports.comports()
+        return [p.device for p in ports]
+
+source = Source()
+
+@app.route("/", methods=["GET"])
 def index():
-    if request.method == "GET":
-        return render_template("index.html")
+    return render_template("index.html")
+
+@app.route("/api/status", methods=["GET"])
+def api_status():
+    return jsonify(status=source.status())
+
+@app.route("/api/ports")
+def api_ports():
+    return jsonify(source.list_ports())
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5002)
