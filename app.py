c5336c5ac50d955f9d336d228d1a081d5414585c
rodrigoniak <rodrigoniak@gmail.com>
2026-03-22 04:14:57 -0300

    feat: add connect

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, render_template, request
+import minimalmodbus
 import serial.tools.list_ports
 
 app = Flask(__name__)
@@ -6,9 +7,21 @@ app = Flask(__name__)
 class Source:
     def status(self):
         return "DISCONNECTED"
+        
     def list_ports(self):
         ports = serial.tools.list_ports.comports()
         return [p.device for p in ports]
+        
+    def connect(self, port):
+        instrument = minimalmodbus.Instrument(port, 1)
+        instrument.serial.baudrate = 9600
+        instrument.serial.bytesize = 8
+        instrument.serial.timeout = 0.5
+        instrument.mode = minimalmodbus.MODE_RTU
+        
+        v_set = int(2.5 * 100)
+        instrument.write_register(0, v_set, functioncode=6) 
+        print(port)
 
 source = Source()
 
@@ -23,6 +36,16 @@ def api_status():
 @app.route("/api/ports")
 def api_ports():
     return jsonify(source.list_ports())
+
+@app.route("/api/connect", methods=["POST"])
+def api_connect():
+    port = request.json["port"]
+
+    try:
+        source.connect(port)
+        return jsonify(success=True)
+    except Exception as e:
+        return jsonify(success=False, error=str(e))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5002)
