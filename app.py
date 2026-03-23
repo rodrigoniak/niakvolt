1eca58bc57d8452f712df178fcaa3ae5455ec24f
rodrigoniak <rodrigoniak@gmail.com>
2026-03-23 16:22:51 -0300

    feat: add status on-off

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask, jsonify, render_template, request
 import minimalmodbus
+import threading
 import serial.tools.list_ports
 
 app = Flask(__name__)
 
 class Source:
+    def __init__(self):
+        self.instrument = None
+        self.lock = threading.Lock()
+        self.port = None
+
     def status(self):
+        if self.instrument:
+            s = self.instrument.read_register(1, functioncode=3)
+            if s == 1:
+                return "ON"
+            else:
+                return "OFF"
         return "DISCONNECTED"
         
     def list_ports(self):
@@ -13,14 +25,16 @@ class Source:
         return [p.device for p in ports]
         
     def connect(self, port):
-        instrument = minimalmodbus.Instrument(port, 1)
-        instrument.serial.baudrate = 9600
-        instrument.serial.bytesize = 8
-        instrument.serial.timeout = 0.5
-        instrument.mode = minimalmodbus.MODE_RTU
+        self.port = port
+        self.instrument = minimalmodbus.Instrument(port, 1)
+        self.instrument.serial.baudrate = 9600
+        self.instrument.serial.bytesize = 8
+        self.instrument.serial.timeout = 0.5
+        self.instrument.mode = minimalmodbus.MODE_RTU
         
         v_set = int(2.5 * 100)
-        instrument.write_register(0, v_set, functioncode=6) 
+        self.instrument.write_register(0, v_set, functioncode=6)
+        self.instrument.write_register(1, 1, functioncode=6)
         print(port)
 
 source = Source()
@@ -31,7 +45,7 @@ def index():
 
 @app.route("/api/status", methods=["GET"])
 def api_status():
-    return jsonify(status=source.status())
+    return jsonify(status=source.status(), port=source.port)
 
 @app.route("/api/ports")
 def api_ports():
@@ -40,7 +54,6 @@ def api_ports():
 @app.route("/api/connect", methods=["POST"])
 def api_connect():
     port = request.json["port"]
-
     try:
         source.connect(port)
         return jsonify(success=True)
